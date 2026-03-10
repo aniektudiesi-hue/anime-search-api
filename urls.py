4bab25f4aa8c641d6a680ee8cfa71fce1c21f57b
aniektudiesi-hue <aniektudiesi@gmail.com>
2026-03-10 18:27:08 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from anime.views import anime_search
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/anime-search/', anime_search, name='anime_search'),
-]
