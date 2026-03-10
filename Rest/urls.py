fff5992a8f64af758f00a529722b35c5669c455c
aniektudiesi-hue <aniektudiesi@gmail.com>
2026-03-10 19:06:23 +0530

    Update urls.py

--- Rest/urls.py
+++ Rest/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('',views.home,name='home'),
+   
     #path('stream/',views.stream,name='stream'),
 ]
