2ab376a67e63035fe178d908bc2d1be26033775a
aniektudiesi-hue <aniektudiesi@gmail.com>
2026-03-15 17:01:59 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import anime_search
+from .views import anime_search,episode_detail,get_stream
 from django.contrib.auth import views as auth_views
 urlpatterns =  [
 
     path('search/',anime_search),
+    path('list/<int:anime_id>',episode_detail),
+    path('stream/<slug:episode_slug>',get_stream),
+
 ]
